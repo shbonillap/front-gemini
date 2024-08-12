52a4606198b6fdc64eb67c49b2ef9d70884e4463
SergioRBurdalo <>
2024-08-12 07:50:19 +0200

    add functionalitie to new summary button

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -8,19 +8,70 @@ const Resume = ({ filename }) => {
     const [resume, setResume] = useState("");
     const [viewmore, setViewmore] = useState(false);
     const [isLoading, setLoading] = useState(false); // Estado para gestionar la carga
-
+    
     useEffect(() => {
         if (filename) {
-            setLoading(true); // Mostrar Skeleton mientras se carga el contenido
-            if (localStorage.getItem(filename) && !viewmore) {
-                const storedResume = localStorage.getItem(filename);
-                renderMarkdown(storedResume);
-                setLoading(false);
-            } else {
-                fetchResume();
+            setLoading(true);
+        if(localStorage.getItem(filename) && !viewmore){
+            renderMarkdown(localStorage.getItem(filename));
+            setLoading(false);
+            // localStorage.setItem(filename, response.data);
+        }
+        else{
+            if (filename && !viewmore) {
+                setLoading(true);
+                axios
+                    .get(`http://localhost:3000/resume/${filename}`)
+                    .then((response) => {
+                        renderMarkdown(response.data);
+                        localStorage.setItem(filename, response.data);
+                        setLoading(false);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+    
+            }
+            if (viewmore) {
+                setLoading(true);
+                axios
+                    .get(`http://localhost:3000/resume/${filename}/${localStorage.getItem(filename)}`)
+                    .then((response) => {
+                        console.log(response.data)
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
             }
         }
+    }
     }, [filename, viewmore]);
+
+const newSummary = () =>{
+    setLoading(true);
+    if (filename && !viewmore) {
+        axios
+            .get(`http://localhost:3000/resume/${filename}`)
+            .then((response) => {
+                setResume(response.data);
+                localStorage.setItem(filename, response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+    }
+    if (viewmore) {
+        axios
+            .get(`http://localhost:3000/resume/${filename}/${localStorage.getItem(filename)}`)
+            .then((response) => {
+                console.log(response.data)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+}
 
     const fetchResume = () => {
         const url = viewmore
@@ -77,46 +128,44 @@ const Resume = ({ filename }) => {
 
     return (
         <div>
-            <p style={{ fontWeight: "bold", fontSize: "30px" }}>Summary</p>
-            <hr />
-            <div>
-                <div style={{ float: "right", display: "inline-block" }}>
-                    <button
-                        className="w-full py-2 semi-rounded text-customGreen bg-white hover:bg-white-800 hover:border-green-950 mt-4"
-                        style={{ width: "150px", marginRight: "8px" }}
-                        onClick={() => setViewmore(true)}
-                    >
-                        <svg style={{ float: "left" }} xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-                            <path fill="#047857" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z" />
-                        </svg>
-                        <p>More info</p>
-                    </button>
-                    <button
-                        className="w-full py-2 semi-rounded text-customGreen bg-white hover:bg-white-800 hover:border-green-950 mt-4"
-                        style={{ width: "150px", marginRight: "8px" }}
-                        onClick={() => DownloadPDF(filename)}
-                    >
-                        <svg style={{ float: "left" }} xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-                            <path fill="#047857" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75m-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75" />
-                        </svg>
-                        <p>Export PDF</p>
-                    </button>
-                    <button
-                        className="w-full py-2 semi-rounded text-white bg-customGreen hover:bg-green-800 hover:border-green-950 mt-4"
-                        style={{ width: "150px" }}
-                    > <p>New summary</p>
-                    </button>
-                </div>
-                <div style={{ display: "inline-block", marginTop: "10px" }}>
-                    {isLoading ? (
-                        <Skeleton />
-                    ) : (
-                        <div dangerouslySetInnerHTML={{ __html: resume }} />
-                    )}
-                </div>
+            <p className="font-bold text-2xl">Summary</p>
+            <hr className="my-4" />
+            <div className="flex justify-end space-x-4 mb-5">
+                <button
+                    className="py-2 px-4 rounded text-customGreen bg-white border border-customGreen hover:bg-gray-100"
+                    onClick={() => setViewmore(true)}
+                >
+                    <svg className="inline-block mr-2" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
+                        <path fill="#047857" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z" />
+                    </svg>
+                    More info
+                </button>
+                <button
+                    className="py-2 px-4 rounded text-customGreen bg-white border border-customGreen hover:bg-gray-100"
+                    onClick={() => DownloadPDF(filename)}
+                >
+                    <svg className="inline-block mr-2" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
+                        <path fill="#047857" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75m-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75" />
+                    </svg>
+                    Export PDF
+                </button>
+                <button
+                    className="py-2 px-4 rounded text-white bg-customGreen hover:bg-green-800"
+                    onClick={newSummary}
+                >
+                    New summary
+                </button>
+            </div>
+            <div className="mt-5">
+                {isLoading ? (
+                    <Skeleton />
+                ) : (
+                    <div dangerouslySetInnerHTML={{ __html: resume }} />
+                )}
             </div>
         </div>
     );
+    
 }
 
 export default Resume;
