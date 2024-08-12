904eb659aa64912e54c2ef53c7cc79b395eb8f1e
Renato PL <renapl90@gmail.com>
2024-08-12 10:18:49 +0200

    Document dorpdown
    Escondemos la flecha para desplegar documento si no hay nada subido. Y por defecto lo dejamos desplegado para que se vea qué documento estamos revisando

--- src/components/documentDropdown.jsx
+++ src/components/documentDropdown.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const DocumentDropdown = ({ hideAll, setFilename }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   const filename = localStorage.getItem("file");
   const navigate = useNavigate();  // Definir el hook de navegación
 
@@ -31,11 +31,13 @@ const DocumentDropdown = ({ hideAll, setFilename }) => {
               <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
               </svg>
-              <span className="ml-2">Documents</span>
+              <span className="ml-2">Document</span>
             </div>
-            <svg className={`h-4 w-4 transform transition-transform ${isOpen ? 'rotate-180' : 'rotate-0'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-            </svg>
+            {filename &&
+              <svg className={`h-4 w-4 transform transition-transform ${isOpen ? 'rotate-180' : 'rotate-0'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+              </svg>
+            }
           </button>
           {isOpen && filename && (
   <div className="mt-2 flex items-center">
